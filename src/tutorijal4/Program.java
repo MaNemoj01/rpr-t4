d8f9a527ed4ddc095d5fa63b6a497f376a91a960
MaNemoj <manemoj01@gmail.com>
2018-11-10 13:10:48 +0100

    Dodane metode dodaj semestar u studij

--- src/tutorijal4/Program.java
+++ src/tutorijal4/Program.java
@@ -4,6 +4,8 @@ public class Program {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Studij RI = new Studij("Racunarstvo i informatika");
+
     }
 }
--- src/tutorijal4/Studij.java
+++ src/tutorijal4/Studij.java
@@ -3,6 +3,42 @@ package tutorijal4;
 public class Studij {
     private Semetar[] spisakSemestara;
     private String imeStudija;
+    private int brojSemestara;
+
+    public Studij(String ime){
+        setImeStudija(ime); setBrojSemestara(0); setSpisakSemestara(new Semetar[100]);
+    }
+
+    public void dodajSemestar(Semetar s){
+        getSpisakSemestara()[getBrojSemestara()]=s;
+        setBrojSemestara(getBrojSemestara()+1);
+    }
+
+    public Semetar[] getSpisakSemestara() {
+        return spisakSemestara;
+    }
+
+    public void setSpisakSemestara(Semetar[] spisakSemestara) {
+        this.spisakSemestara = spisakSemestara;
+    }
+
+    public String getImeStudija() {
+        return imeStudija;
+    }
+
+    public void setImeStudija(String imeStudija) {
+        this.imeStudija = imeStudija;
+    }
+
+    public int getBrojSemestara() {
+        return brojSemestara;
+    }
+
+    public void setBrojSemestara(int brojSemestara) {
+        this.brojSemestara = brojSemestara;
+    }
 
 
+
+    // napraviti da je ovo bazna klasa i napraviti izvedenu klasu  Plan Studija i u tom ce biti sadrzani obavezni predmet i plus predmeti na koje se student upise kao izborni!!
 }
